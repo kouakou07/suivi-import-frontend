fdef30be6cb3d4179b7ccd940c9b97089d4cb17e
franciskouakou174 <franciskouakou174@gmail.com>
2025-07-06 00:46:59 +0000

    US-INITIALISER: Initialisation de mon projet

---
+++ src/test/java/com/example/suivie_importBackend/SuivieImportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suivie_importBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuivieImportBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
